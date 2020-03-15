1332ec827cf8c9663dd33cce068f7a83b9d89225
Rajat Mishra <38719209+RajatADP@users.noreply.github.com>
2020-03-15 16:19:18 +0530

    first commit

---
+++ main/java/com/learning/clientDataToWebApp/ClientDataToWebApp.java
@@ -0,0 +1,15 @@
+package com.learning.clientDataToWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ClientDataToWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientDataToWebApp.class, args);
+
+	}
+
+}
---
+++ main/java/com/learning/clientDataToWebApp/ClientToWebAppController.java
@@ -0,0 +1,20 @@
+package com.learning.clientDataToWebApp;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ClientToWebAppController {
+
+	@RequestMapping("/client")
+	public String clientToWebApp(HttpServletRequest req) {
+		HttpSession httpSession = req.getSession();
+		String clientName = req.getParameter("cname");
+		System.out.println("********************************" +clientName);
+		httpSession.setAttribute("clientName", clientName);
+		return "clientToWeb";
+	}
+}
