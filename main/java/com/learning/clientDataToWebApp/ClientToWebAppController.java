409c6d79a6e8ff28c9790800ba1c262b796a030b
Rajat Mishra <38719209+RajatADP@users.noreply.github.com>
2020-03-15 19:40:07 +0530

    hh

--- main/java/com/learning/clientDataToWebApp/ClientToWebAppController.java
+++ main/java/com/learning/clientDataToWebApp/ClientToWebAppController.java
@@ -6,14 +6,14 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@Controller
-//public class ClientToWebAppController {
-//
-//	@RequestMapping("/client")
-//	public String clientToWebApp(HttpServletRequest req) {
-//		HttpSession httpSession = req.getSession();
-//		String clientName = req.getParameter("cname");
-//		httpSession.setAttribute("clientName", clientName);
-//		return "clientToWeb";
-//	}
-//}
+@Controller
+public class ClientToWebAppController {
+
+	@RequestMapping("/client")
+	public String clientToWebApp(HttpServletRequest req) {
+		HttpSession httpSession = req.getSession();
+		String clientName = req.getParameter("cname");
+		httpSession.setAttribute("clientName", clientName);
+		return "clientToWeb";
+	}
+}
